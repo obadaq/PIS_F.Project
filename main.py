284f22154192a7c1111c1a1bee1d2fcf0bfa8395
Eng. Obada Qawasmi <obadaq@ppu.edu>
2023-01-10 07:53:54 +0200

    second commit

--- main.py
+++ main.py
@@ -9,17 +9,40 @@ class student_report:
     
     def __init__(self,marks_df_path):
         
+
         self.marks_df = pd.read_excel(marks_df_path)
-        self.std_names = self.marks_df['Name']
-
-
+        self.std_names = self.marks_df['Name'] 
+        self.rubrics = self.marks_df.iloc[0]
+        unwated_elm =  ('Email', 'Name','Rubric', 'Total Grade')
+        for elm in unwated_elm:
+            self.rubrics.pop(elm)
+        
+       
     def get_names(self):
         return self.std_names
 
+    def fet_rubrics(self):
+        return self.rubrics
 
     def get_std_marks(self,std_name):
-        self.marks_df = self.marks_df.set_index('Name')
-        return self.marks_df.loc[std_name]
+        m_df = self.marks_df
+        m_df = m_df.set_index('Name')
+        std_marks = dict(m_df.loc[std_name])
+        unwated_elm =  ('Email','Rubric')
+        for elm in m_df:
+            std_marks.pop(elm)
+        return std_marks
 
-    def get_nulls(self):
+    def unsupmitted(self,std_name):
+        m_df = self.marks_df
+        m_df = m_df.set_index('Name')
+        rubric = dict(m_df.loc[std_name].isnull())
+        unwated_elm =  ('Email','Rubric', 'Total Grade')
+        for elm in unwated_elm:
+            rubric.pop(elm)
+        nulls = []
+        for item in rubric:
+            if rubric.get(item):
+                nulls.append(item)
+        return nulls
         
--- test.py
+++ test.py
@@ -1,10 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 marks_df = pd.read_excel("Marks\Class1_Marks.xlsx")
 stdnames = marks_df['Name']
+'''
 marks_df = marks_df.set_index('Name')
+dict1 = dict(marks_df.iloc[4].isnull())
+del_dict1 = ('Email','Rubric', 'Total Grade')
 
-##print(marks_df.iloc[1].isnull()['HW2'] )
-print(stdnames)
+for d in del_dict1:
+    dict1.pop(d)
+
+print(dict1 )
+
+
+
+nulls = []
+for item in dict1:
+    if dict1.get(item):
+        nulls.append(item)
+
+print(nulls)
+
+'''
+ru = marks_df.columns.values.tolist()
+del ru[0:3]
+ru.remove("Total Grade")
+
+
+rubrics = marks_df.iloc[0]
+unwated_elm =  ('Email', 'Name','Rubric', 'Total Grade')
+for elm in unwated_elm:
+    rubrics.pop(elm)
+print(rubrics)
+
+plt.pie(rubrics, radius=1,labels=rubrics.keys(), autopct="%1.2f%%", wedgeprops=dict(width=1, edgecolor='white'))
+plt.title("Weight of Course Activities")
+plt.savefig('plot.png', dpi=300, bbox_inches='tight')
+plt.show()
